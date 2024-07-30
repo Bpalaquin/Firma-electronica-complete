7b6adc869bcbc4943b3cf1f9697bfd6faecdd2f7
Bpalaquin <Beldrian05@hotmail.com>
2024-07-30 13:24:07 -0500

    Create services.ts
    Peticiones de consultas de datos.

---
+++ services.ts
@@ -0,0 +1,34 @@
+import { api } from "src/boot/axios";   
+import { notify } from "src/boot/notify-defaults";
+import { IUserRegisterData } from "./interfaces";
+
+export const fetchClients = async() => {
+    try {
+      const response = await api.get('/clients'); 
+      return response;
+    } catch (error) {
+      console.error('Error al obtener clientes:', error);
+      return [];
+    }
+};
+
+export const registerUserService = async (clientsData: IUserRegisterData): Promise<any> => {
+    try{
+        return await api.post('/clients',clientsData);
+    }catch (error: any){
+        console.error(error);
+        notify(`Ocurrió un error obtener los clientes: ${error?.response?.data?.message || 'Error desconocido'}`, 'negative');
+        throw error;
+    }
+};
+
+export const updateUserService = async(clientsData: IUserRegisterData): Promise<any> =>{
+    let id = clientsData.is_da;
+    //Update by ID
+    return null;
+};
+
+export const deleteUserService = async(id: string): Promise<any> =>{
+    //delet by ID
+    return null;
+};
