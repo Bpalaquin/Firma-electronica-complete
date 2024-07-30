7a93844ce0e11a516d46eec9af604a3af4f03369
Bpalaquin <Beldrian05@hotmail.com>
2024-07-30 13:22:44 -0500

    Create interfaces.ts
    Datos para enviar a la API

---
+++ interfaces.ts
@@ -0,0 +1,11 @@
+export interface IUserRegisterData{
+    username: string;
+    domain: string;
+    name: string;
+    last_name: string;
+    email: string;
+    password: string;
+    role_id: string;
+    is_da: true;
+    image_url: string;
+}
